80a84031fb44f652e543951f4c65dcd8239b83b1
kolynzb <collinsbenda360@gmail.com>
2022-07-07 06:28:10 +0300

    Add Sccafold to recommended page

---
+++ lib/screens/food/recommended_food_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecommendedFoodDetail extends StatelessWidget {
+  const RecommendedFoodDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: CustomScrollView());
+  }
+}
